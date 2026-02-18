aa4780c7491d84e3372932c6cde23c9aee3fca3c
joao souza <joao.souza.lauro@gmail.com>
2026-02-18 12:44:57 -0300

    primeira aula de programaçao

---
+++ aula01.py
@@ -0,0 +1,7 @@
+numero = 250 # variavel int
+numero_quebrado = 2.56 # variavel float
+joao_ta_dormindo = True # variável booleana
+texto = "aula inicial de programação" # variavel de texto
+
+
+print(joao_ta_dormindo)
