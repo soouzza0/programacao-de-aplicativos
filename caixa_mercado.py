fb32df40164e064d457625d42bf96699d5bace09
unknown <joao.souza.lauro@gmail.com>
2026-02-24 11:52:47 -0300

    24/02/2026

---
+++ caixa_mercado.py
@@ -0,0 +1,10 @@
+produto = (input("digite o nome do seu produto:"))
+valor = float(input("digite o valor do seu produto:"))
+quantidade = int(input("digite a quantidade do produto:"))
+
+total = valor * quantidade
+
+print("produto:" , produto)
+print("quantidade:" , quantidade)
+print("valor:" , valor)
+print("total:" , total)
---
+++ rpg.py
@@ -0,0 +1,11 @@
+nome= (input("escolha o seu nome;"))
+nivel = int(input("escolha o nivel do personagem"))
+ouro = int(input("escolha a sua quantidade de ouro"))
+vida = (input("seu personagem esta vivo ou nao"))
+
+
+
+print("seu nome é:" , nome)
+print("seu nivel é;" , nivel)
+print("sua quantidade de ouro é;" , ouro)
+print("seu status de vida é; " , vida)
