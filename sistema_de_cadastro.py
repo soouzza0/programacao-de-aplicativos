a6af2dcedbf547d960ff79c81d47b92dc7466176
joao <joao.souza.lauro@gmail.com>
2026-06-12 10:08:00 -0300

    12/06

--- sistema_de_cadastro.py
+++ sistema_de_cadastro.py
@@ -4,17 +4,18 @@ conecao = sqlite3. connect('escola.db')
 cursor = conecao.cursor()
 
 cursor.execute('''
-                CREAT TABLE IF NOT EXIST cadastro_alunos(
-                id integrer primary key autoincrement
-                nome text not null,
-                telefone text,
-                turma text,
-                idade_aluno interger,
-                cpf text unique not null 
-                )''')
-nome_aluno = input("digite o nome do aluno")
-idade_aluno = int(input("digite o telefone do aluno: "))
+                CREATE TABLE IF NOT EXISTS cadastro_alunos(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                telefone TEXT,
+                turma TEXT,
+                idade_aluno INTEGER,
+                cpf TEXT UNIQUE NOT NULL)''')
+nome_aluno = input("digite o nome do aluno: ")
+idade_aluno = int(input("digite a idade do aluno: "))
 turma_aluno = input("digite a turma: ")
 telefone_aluno = input("digite o telefone do aluno")
 cpf_aluno = input("digite o cpf do aluno ")
 
+comando_inserir = (f''' INSERT INTO alunos("nome,idade,turma,telefone,cpf")
+                   VALUES('{nome_aluno}', '{telefone_aluno}', '{turma_aluno}', '{idade_aluno}', '{cpf_aluno}''')
