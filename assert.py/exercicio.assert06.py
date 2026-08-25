a424ddecc45ad1f2f91e0c2f7e9b2ef0dde683e0
joao <joao.souza.lauro@gmail.com>
2026-08-25 11:55:27 -0300

    25/08

---
+++ assert.py/exercicio.assert06.py
@@ -0,0 +1,9 @@
+def dobrar(numero):
+ 	return numero * 2
+
+assert dobrar(3) == 6 #p
+assert dobrar(0) == 1 #f
+assert dobrar(-2) == -4 #p
+
+
+# erro na linha 5, pois 0x2 = 0 nao resultado 1 entao da erro 
